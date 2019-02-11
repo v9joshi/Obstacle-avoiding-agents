77ad2d04f704d9a5c7c21a35f7c21acf318c97d0
Varun Joshi <joshi.142@osu.edu>
2019-02-11 13:17:17 -0500

    Root File for the new simulation
    This file will be the main file containing the description and
    parameters for the model. All other simulation related files will be
    called through this one.

---
+++ rootFile.m
@@ -0,0 +1,24 @@
+% This program simulates multiple agents interacting with each other and
+% the environment. The agents have inidivual behaviour designed to 
+% a) Move towards a goal.
+% b) Avoid obstacles in the way.
+% c) Interact with other agents so as to move in a group but also not
+% collide with each other.
+
+% Clear and close everything
+clc; close all; clear all;
+
+% Define some parameters
+
+
+% Define some environment variables
+
+
+
+% Run the simulation
+
+
+
+
+
+% Plot and output some data
